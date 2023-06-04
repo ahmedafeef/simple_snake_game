84ad8195ae6ddba02bfd6d14182f85647a681a5c
ahmed afeef <ahmedafeef1999@gmail.com>
2023-06-04 06:10:02 +0300

    adding more challenges and diffecalty to the game

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ pygame.display.set_caption(WINDOW_TITLE)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # set up the font
 font = pygame.font.SysFont(None, 25)
@@ -22,20 +24,22 @@ font = pygame.font.SysFont(None, 25)
 # set up the clock
 clock = pygame.time.Clock()
 
-# set up the snake
+# set up the Snake
 snake_block_size = 10
 snake_speed = 15
+snake_list = []
+length_of_snake = 5
 
 def snake(snake_block_size, snake_list):
     for x in snake_list:
-        pygame.draw.rect(game_window, BLACK, [x[0], x[1], snake_block_size, snake_block_size])
+        pygame.draw.rect(game_window, BLUE, [x[0], x[1], snake_block_size, snake_block_size])
 
 # set up the game loop
 def game_loop():
     game_over = False
     game_close = False
 
-    # set up the initial position of the snake
+    # set up the initial position of the Snake
     x1 = WINDOW_WIDTH / 2
     y1 = WINDOW_HEIGHT / 2
 
@@ -43,12 +47,37 @@ def game_loop():
     food_x = round(random.randrange(0, WINDOW_WIDTH - snake_block_size) / 10.0) * 10.0
     food_y = round(random.randrange(0, WINDOW_HEIGHT - snake_block_size) / 10.0) * 10.0
 
-    # set up the initial direction of the snake
+    # set up the initial direction of the Snake
     x1_change = 0
     y1_change = 0
 
-    snake_list = []
-    length_of_snake = 1
+    # set up the initial size of the Snake
+    global length_of_snake
+    length_of_snake = 5
+    snake_list.clear()
+
+    # set up the initial speed of the Snake
+    global snake_speed
+    snake_speed = 15
+
+    # set up the obstacles
+    obstacles = []
+    num_obstacles = 5
+    for i in range(num_obstacles):
+        obs_x = round(random.randrange(0, WINDOW_WIDTH - snake_block_size) / 10.0) * 10.0
+        obs_y = round(random.randrange(0, WINDOW_HEIGHT - snake_block_size) / 10.0) * 10.0
+        obs_w = random.randint(1, 3) * snake_block_size
+        obs_h = random.randint(1, 3) * snake_block_size
+        obstacles.append(pygame.Rect(obs_x, obs_y, obs_w, obs_h))
+
+    # set up the power-ups
+    power_ups = []
+    num_power_ups = 3
+    for i in range(num_power_ups):
+        power_up_x = round(random.randrange(0, WINDOW_WIDTH - snake_block_size) / 10.0) * 10.0
+        power_up_y = round(random.randrange(0, WINDOW_HEIGHT - snake_block_size) / 10.0) * 10.0
+        power_up_type = random.choice(["speed", "length"])
+        power_ups.append({"rect": pygame.Rect(power_up_x, power_up_y, snake_block_size, snake_block_size), "type": power_up_type})
 
     # game loop
     while not game_over:
@@ -87,40 +116,75 @@ def game_loop():
                     y1_change = snake_block_size
                     x1_change = 0
 
-        # check if the snake hits the borders
+        # check if the Snake hits the borders or obstacles
         if x1 >= WINDOW_WIDTH or x1 < 0 or y1 >= WINDOW_HEIGHT or y1 < 0:
             game_close = True
 
-        # update the position of the snake
+        for obs in obstacles:
+            if obs.collidepoint(x1, y1):
+                game_close = True
+
+        # update the position of the Snake
         x1 += x1_change
         y1 += y1_change
 
-        # draw the food
+        # draw the background and obstacles
         game_window.fill(WHITE)
+        for obs in obstacles:
+            pygame.draw.rect(game_window, BLACK, obs)
+
+        # draw the food
         pygame.draw.rect(game_window, RED, [food_x, food_y, snake_block_size, snake_block_size])
 
-        # draw the snake
-        snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
+        # update the Snake's length and speed if it eats the food
+        snake_head = [x1, y1]
         snake_list.append(snake_head)
-
         if len(snake_list) > length_of_snake:
             del snake_list[0]
 
-        snake(snake_block_size, snake_list)
-        pygame.display.update()
+        for power_up in power_ups:
+            if power_up["rect"].collidepoint(x1, y1):
+                if power_up["type"] == "speed":
+                    snake_speed += 5
+                elif power_up["type"] == "length":
+                    length_of_snake += 5
+                power_ups.remove(power_up)
 
-        # check if the snake hits the food
         if x1 == food_x and y1 == food_y:
             food_x = round(random.randrange(0, WINDOW_WIDTH - snake_block_size) / 10.0) * 10.0
             food_y = round(random.randrange(0, WINDOW_HEIGHT - snake_block_size) / 10.0) * 10.0
             length_of_snake += 1
 
-        # set up the clock
+            # add new obstacle
+            if len(obstacles) < num_obstacles:
+                obs_x = round(random.randrange(0, WINDOW_WIDTH - snake_block_size) / 10.0) * 10.0
+                obs_y = round(random.randrange(0, WINDOW_HEIGHT - snake_block_size) / 10.0) * 10.0
+                obs_w = random.randint(1, 3) * snake_block_size
+                obs_h = random.randint(1, 3) * snake_block_size
+                obstacles.append(pygame.Rect(obs_x, obs_y, obs_w, obs_h))
+
+            # add new power-up
+            if len(power_ups) < num_power_ups:
+                power_up_x = round(random.randrange(0, WINDOW_WIDTH - snake_block_size) / 10.0) * 10.0
+                power_up_y = round(random.randrange(0, WINDOW_HEIGHT - snake_block_size) / 10.0) * 10.0
+                power_up_type = random.choice(["speed", "length"])
+                power_ups.append({"rect": pygame.Rect(power_up_x, power_up_y, snake_block_size, snake_block_size), "type": power_up_type})
+
+        # update the Snake's position and draw it on the screen
+        snake(snake_block_size, snake_list)
+
+        # update the score
+        score = length_of_snake - 5
+        score_text = font.render("Score: " + str(score), True, BLACK)
+        game_window.blit(score_text, [0, 0])
+
+        # update the display and wait for a short time
+        pygame.display.update()
         clock.tick(snake_speed)
 
-    # quit Pygame
+    # quit Pygame and exit the program
     pygame.quit()
+    quit()
 
+# start the game loop
 game_loop()
